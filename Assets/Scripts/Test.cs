d139cab0a797028aa34abc3677adc708b4b472ae
Swiftcakez <Bernd.Eissing@gmail.com>
2018-11-27 00:30:39 +0100

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	private int Hallo;
+	private int Döfel;
+
+	void Start() 
+	{
+		Hallo = 1;
+		Döfel = 2;
+	}
+}
